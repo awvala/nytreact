d835358a3971884b0011271e367bbfa955236651
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-09-30 14:08:26 -0500

    added moment.js to card

--- react-app/client/src/components/Article/Article.js
+++ react-app/client/src/components/Article/Article.js
@@ -1,10 +1,11 @@
 import React from "react";
+const moment = require('moment');
 
 const Article = props => (
     <div className= "card" id={props._id}>
         <h5 className="card-header">{props.topic}</h5>
         <div className="card-body">
-            <h5 className="card-title" id="date">{props.date}</h5>
+            <h5 className="card-title" id="date">{moment(props.date).format("MMMM Do YYYY")}</h5>
             <p className="card-text" id="snippet">{props.snippet}</p>
             <a href={props.url} className="btn btn-primary" id="articleurl" target="_blank">Read</a>
             <button
