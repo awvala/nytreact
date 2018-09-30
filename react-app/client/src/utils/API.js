ba0545bede349f9179bdbb1a47d52fe49409941e
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-09-29 19:11:41 -0500

    updated utils APi routes

--- react-app/client/src/utils/API.js
+++ react-app/client/src/utils/API.js
@@ -1,9 +1,20 @@
 import axios from "axios";
-const APIKEY = "api-key=82b9bb53e0504de1bbc493474a966560";
-const BASEURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
 
 export default {
-  search: function(query) {
-    return axios.get(BASEURL + query + APIKEY);
+  // Gets all articles
+  getArticles: function() {
+    return axios.get("/api/articles");
+  },
+  // Gets the article with the given id
+  getArticles: function(id) {
+    return axios.get("/api/articles/" + id);
+  },
+  // Deletes the article with the given id
+  deleteArticles: function(id) {
+    return axios.delete("/api/articles/" + id);
+  },
+  // Saves an article to the database
+  saveArticles: function(articleData) {
+    return axios.post("/api/articles", articleData);
   }
 };
