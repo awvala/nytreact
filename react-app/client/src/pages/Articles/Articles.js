2661708f2b3bf9f5f5424752deba718dcab30385
Andrew Vala <38055092+awvala@users.noreply.github.com>
2018-09-29 19:47:37 -0500

    updated functions on articles page

--- react-app/client/src/pages/Articles/Articles.js
+++ react-app/client/src/pages/Articles/Articles.js
@@ -3,15 +3,17 @@ import Jumbotron from "../../components/Jumbotron";
 import API from "../../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../../components/Grid";
-import { Input, FormBtn } from "../../components/Form";
+import { Input, FormBtn ,TextArea} from "../../components/Form";
 
 
 class Articles extends Component {
     state = {
         articles: [],
-        title: "",
-        url: "",
-        date: "",
+        topic: "",
+        startYear: "",
+        endYear: "",
+        page: 0,
+        currentSearch: ""
     };
 
     componentDidMount() {
@@ -28,6 +30,22 @@ class Articles extends Component {
 
     // Skipping delete function
 
+    saveArticle = (article) => {
+        let newArticle = {
+            date: article.pub_date,
+            title: article.headline.main,
+            url: article.web_url,
+            summary: article.snippet
+        }
+
+        API.saveArticle(newArticle).then(articles => {
+          //removing the saved article from the articles in state
+          let unsavedArticles = this.state.articles.filter(article => article.headline.main !== newArticle.title)
+          this.setState({articles: unsavedArticles})
+        })
+        .catch(err => console.log(err));
+    }
+
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
@@ -37,17 +55,63 @@ class Articles extends Component {
 
     handleFormSubmit = event => {
         event.preventDefault();
-        if (this.state.title && this.state.url && this.state.date) {
-            API.saveArticles({
-                title: this.state.title,
-                url: this.state.url,
-                sate: this.state.date
-            })
-                .then(res => this.loadArticles())
-                .catch(err => console.log(err));
-        }
+        let {topic, startYear, endYear} = this.state;
+        let query = {topic, startYear, endYear}
+        this.getArticles(query)
     };
 
+    getArticles = query => {
+        if (query.topic !== this.state.currentSearch.topic || query.startYear !== this.state.currentSearch.startYear || query.endYear !== this.state.currentSearch.endYear) {
+            this.setState ({
+                articles: []
+            })
+        }
+
+        let {topic, startYear, endYear } = query;
+        let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?sort=newest&page=${this.state.page}`;
+        let APIkey = "&api-key=82b9bb53e0504de1bbc493474a966560"
+
+        if(topic.indexOf(' ') >= 0) {
+            topic = topic.replace(/\s/g, '+');
+        }
+
+        if (topic) {
+        queryURL+= `&fq=${topic}`
+        }
+
+        if(sYear) {
+        queryURL+= `&begin_date=${startYear}`
+        }
+
+        if(eYear) {
+        queryURL+= `&end_date=${endYear}`
+        }
+
+        queryURL+=APIkey;
+
+        API.queryNYT(queryUrl).then(results => {
+          this.setState({
+            articles: [...this.state.results, ...results.data.response.docs],
+            currentSearch: query,
+            topic: '',
+            startYear: '',
+            endYear: ''
+          });
+      })
+      .catch(err=> console.log(err))
+    }
+
+    getAdditionalResults = () => {
+        let { topic, startYear, endYear} = this.state.currentSearch;
+        let query = { topic, startYear, endYear }
+        let page = this.state.page;
+        page++
+        this.setState({
+            page: page
+        }, function () {
+          this.getArticles(query)
+        });
+      };
 }
 
 export default Articles;
